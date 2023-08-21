652e6771283b0029f39f5b60011bfd42fa62ae3b
LAPTOP-0RLF8TG9\Jorge Andres <jorgeandresop1@gmail.com>
2023-08-21 12:49:12 -0500

    Primer comit

---
+++ trabajo_2/src/paquete1/Proyecto.java
@@ -0,0 +1,19 @@
+package paquete1;
+
+public class Proyecto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//Creamos un menú de opciones para que se muestre en pantalla y el usuario elija lo que quiere	
+	System.out.println("------------------------------");
+	System.out.println("¿Que operacion quieres hacer?");
+	System.out.println("1. Insertar alumno");
+	System.out.println("2. Eliminar alumno");
+	System.out.println("3. Modificar alumno");
+	System.out.println("4. Consultar alumno");
+	System.out.println("5. Terminar programa");
+	System.out.println("------------------------------");
+
+	}
+
+}
